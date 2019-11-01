e37381878fb13691bce639bd3c95d36a3bcf310e
ali qamar <ali.qamar@bsqptech.com>
2019-11-01 20:15:52 +0500

    Main Screen UI

---
+++ iFollow/Controllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  iFollow
+//
+//  Created by Shahzeb siddiqui on 01/11/2019.
+//  Copyright © 2019 Shahzeb siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var btnSignup: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnLoginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnSignupTapped(_ sender: UIButton) {
+    }
+    
+    
+}
