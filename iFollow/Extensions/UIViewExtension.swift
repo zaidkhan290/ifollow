6ffa95c5287ab525b4370a597c3bdf580cbb9990
ali qamar <ali.qamar@bsqptech.com>
2019-11-04 21:37:24 +0500

    Some file commit due to internet connectivity

---
+++ iFollow/Extensions/UIViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewExtension.swift
+//  iFollow
+//
+//  Created by Shahzeb siddiqui on 04/11/2019.
+//  Copyright © 2019 Shahzeb siddiqui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    
+    func dropShadow(color: UIColor){
+        self.backgroundColor = color
+        self.layer.masksToBounds = false
+        self.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.layer.rasterizationScale = UIScreen.main.scale
+        self.layer.shadowRadius = 4.0
+        self.layer.shadowOpacity = 1.0
+    }
+}
