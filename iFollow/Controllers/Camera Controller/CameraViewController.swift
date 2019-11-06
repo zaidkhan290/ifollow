6cab78bfa26a7a5df1d567b22ce342204aec08b2
ali qamar <ali.qamar@bsqptech.com>
2019-11-06 20:05:03 +0500

    Camera Controller with live camera

--- iFollow/Controllers/Camera Controller/CameraViewController.swift
+++ iFollow/Controllers/Camera Controller/CameraViewController.swift
@@ -7,20 +7,77 @@
 //
 
 import UIKit
+import Photos
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var cameraView: UIImageView!
     @IBOutlet weak var btnCapture: UIButton!
     @IBOutlet weak var btnFlash: UIButton!
     @IBOutlet weak var btnRotate: UIButton!
     
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            
+            stillImageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+            
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
 
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        //Step12
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+            //Step 13
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.frame
+            }
+        }
+    }
+    
     //MARK:- Actions
     
     @IBAction func btnBackTapped(_ sender: UIButton) {
@@ -31,8 +88,10 @@ class CameraViewController: UIViewController {
     }
     
     @IBAction func btnFlashTapped(_ sender: UIButton) {
+        
     }
     
     @IBAction func btnRotateTapped(_ sender: UIButton) {
+        
     }
 }
