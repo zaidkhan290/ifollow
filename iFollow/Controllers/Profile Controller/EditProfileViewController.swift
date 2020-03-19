e385ec297deed61eff34369cee8184ec8c9fa2cf
ali qamar <ali.qamar@bsqptech.com>
2020-03-19 16:25:56 +0500

    Murtaza Update profile

--- iFollow/Controllers/Profile Controller/EditProfileViewController.swift
+++ iFollow/Controllers/Profile Controller/EditProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import SDWebImage
 import Loaf
 import RealmSwift
+import SKCountryPicker
 
 class EditProfileViewController: UIViewController {
 
@@ -22,6 +23,18 @@ class EditProfileViewController: UIViewController {
     var textFieldPlaceholders = [String]()
     var textFieldImages = [String]()
     var selectedDate = ""
+    var dobDatePicker = UIDatePicker()
+    
+    var firstName = ""
+    var lastName = ""
+    var dob = ""
+    var username = ""
+    var userGender = ""
+    var shortBio = ""
+    var hobby = ""
+    var userCountry = ""
+    var zipCode = ""
+    var userModel = UserModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,12 +51,24 @@ class EditProfileViewController: UIViewController {
         editProfileTableView.register(genderCellNib, forCellReuseIdentifier: "EditProfileGenderTableViewCell")
         editProfileTableView.register(doneButtonCellNib, forCellReuseIdentifier: "EditProfileSaveButtonTableViewCell")
         
-        textFieldPlaceholders = ["", "First Name", "Last Name", "mm/dd/yy", "Username", "", "Short Bio", "Hobby", "Country", "Zip Code", "City"]
-        textFieldImages = ["", "username-1", "username-1", "calendar", "username-1", "", "username-1", "hobby", "country", "zip-code", "city"]
+        textFieldPlaceholders = ["", "First Name", "Last Name", "mm/dd/yy", "Username", "", "Short Bio", "Hobby", "Country", "Zip Code"]
+        textFieldImages = ["", "username-1", "username-1", "calendar", "username-1", "", "username-1", "hobby", "country", "zip-code"]
         
         imagePicker.allowsEditing = true
         imagePicker.mediaTypes = ["public.image" /*"public.movie"*/]
         imagePicker.delegate = self
+        
+        dobDatePicker.datePickerMode = .date
+        userModel = UserModel.getCurrentUser()!
+        userGender = userModel.userGender
+        firstName = userModel.userFirstName
+        lastName = userModel.userLastName
+        dob = userModel.userDOB
+        username = userModel.username
+        shortBio = userModel.userShortBio
+        hobby = userModel.userHobby
+        userCountry = userModel.userCountry
+        zipCode = userModel.userZipCode
         
     }
    
@@ -53,11 +78,52 @@ class EditProfileViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @objc func handleDatePicker(sender: UIDatePicker) {
+    @objc func textfieldDidBeginEditing(_ sender: UITextField){
+        if (sender.tag == 3){
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd MMM yyyy"
+            selectedDate = dateFormatter.string(from: dobDatePicker.date)
+            let cell = self.editProfileTableView.cellForRow(at: IndexPath(row: 3, section: 0)) as! EditProfileTextFieldsTableViewCell
+            cell.txtField.text = selectedDate
+            dob = selectedDate
+            dobDatePicker.addTarget(self, action: #selector(handleDatePicker), for: .valueChanged)
+        }
+    }
+    
+    @objc func handleDatePicker() {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd MMM yyyy"
-        selectedDate = dateFormatter.string(from: sender.date)
-        self.editProfileTableView.reloadData()
+        selectedDate = dateFormatter.string(from: dobDatePicker.date)
+        let cell = self.editProfileTableView.cellForRow(at: IndexPath(row: 3, section: 0)) as! EditProfileTextFieldsTableViewCell
+        cell.txtField.text = selectedDate
+        dob = selectedDate
+    }
+    
+    @objc func textFieldTextChanged(_ sender: UITextField){
+        if (sender.tag == 1){
+            firstName = sender.text!
+        }
+        else if (sender.tag == 2){
+            lastName = sender.text!
+        }
+        else if (sender.tag == 3){
+            dob = sender.text!
+        }
+        else if (sender.tag == 4){
+            username = sender.text!
+        }
+        else if (sender.tag == 6){
+            shortBio = sender.text!
+        }
+        else if (sender.tag == 7){
+            hobby = sender.text!
+        }
+        else if (sender.tag == 8){
+            userCountry = sender.text!
+        }
+        else if (sender.tag == 9){
+            zipCode = sender.text!
+        }
     }
     
     func openImagePicker(){
@@ -113,12 +179,72 @@ class EditProfileViewController: UIViewController {
         }
         
     }
+    
+    func updateUserProfileWithRequest(){
+        
+        let params = ["first_name": firstName,
+                      "last_name": lastName,
+                      "date_of_birth": dob,
+                      "username": username,
+                      "gender": userGender,
+                      "short_bio": shortBio,
+                      "hobby": hobby,
+                      "country": userCountry,
+                      "zip_code": zipCode,
+                      "email": userModel.userEmail]
+        
+        Utility.showOrHideLoader(shouldShow: true)
+        
+        API.sharedInstance.executeAPI(type: .updateProfile, method: .post, params: params) { (status, result, message) in
+            
+            DispatchQueue.main.async {
+                Utility.showOrHideLoader(shouldShow: false)
+                
+                if (status == .success){
+                    
+                    let realm = try! Realm()
+                    try! realm.safeWrite {
+                        if let user = UserModel.getCurrentUser(){
+                            user.userFirstName = self.firstName
+                            user.userLastName = self.lastName
+                            user.userDOB = self.dob
+                            user.username = self.username
+                            user.userGender = self.userGender
+                            user.userShortBio = self.shortBio
+                            user.userHobby = self.hobby
+                            user.userCountry = self.userCountry
+                            user.userZipCode = self.zipCode
+                        }
+                    }
+                    
+                    Loaf(message, state: .success, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                    
+                }
+                else if (status == .failure){
+                    Loaf(message, state: .error, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                        
+                    }
+                }
+                else if (status == .authError){
+                    Loaf(message, state: .error, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                        Utility.logoutUser()
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+    }
 }
 
 extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 12
+        return 11
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -137,9 +263,23 @@ extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate{
         }
         else if (indexPath.row == 5){
             let cell = tableView.dequeueReusableCell(withIdentifier: "EditProfileGenderTableViewCell", for: indexPath) as! EditProfileGenderTableViewCell
+            cell.delegate = self
+            
+            if (userGender == ""){
+                cell.manImage.layer.borderColor = UIColor.clear.cgColor
+                cell.girlImage.layer.borderColor = UIColor.clear.cgColor
+            }
+            else if (userGender == "male"){
+                cell.manImage.layer.borderColor = UIColor.black.cgColor
+                cell.girlImage.layer.borderColor = UIColor.clear.cgColor
+            }
+            else{
+                cell.girlImage.layer.borderColor = UIColor.black.cgColor
+                cell.manImage.layer.borderColor = UIColor.clear.cgColor
+            }
             return cell
         }
-        else if (indexPath.row == 11){
+        else if (indexPath.row == 10){
             let cell = tableView.dequeueReusableCell(withIdentifier: "EditProfileSaveButtonTableViewCell", for: indexPath) as! EditProfileSaveButtonTableViewCell
             cell.delegate = self
             return cell
@@ -148,18 +288,49 @@ extension EditProfileViewController: UITableViewDataSource, UITableViewDelegate{
             let cell = tableView.dequeueReusableCell(withIdentifier: "EditProfileTextFieldsTableViewCell", for: indexPath) as! EditProfileTextFieldsTableViewCell
             Utility.setTextFieldPlaceholder(textField: cell.txtField, placeholder: textFieldPlaceholders[indexPath.row], color: Theme.editProfileTextFieldColor)
             cell.icon.image = UIImage(named: textFieldImages[indexPath.row])
+            cell.txtField.tag = indexPath.row
+            cell.txtField.delegate = self
+            cell.txtField.addTarget(self, action: #selector(textFieldTextChanged(_:)), for: .editingChanged)
             
-            if (indexPath.row == 3){
-                cell.txtField.keyboardType = .numberPad
-                cell.txtField.text = selectedDate
-                cell.txtField.keyboardType = .numberPad
-                let datePicker = UIDatePicker()
-                datePicker.datePickerMode = .date
-                cell.txtField.inputView = datePicker
-                datePicker.addTarget(self, action: #selector(handleDatePicker(sender:)), for: .valueChanged)
-            }
-            else{
+            if (indexPath.row == 1){
+                cell.txtField.text = firstName
                 cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .words
+            }
+            else if (indexPath.row == 2){
+                cell.txtField.text = lastName
+                cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .words
+            }
+            else if (indexPath.row == 3){
+                cell.txtField.text = dob
+                cell.txtField.inputView = dobDatePicker
+                cell.txtField.addTarget(self, action: #selector(textfieldDidBeginEditing(_:)), for: .editingDidBegin)
+            }
+            else if (indexPath.row == 4){
+                cell.txtField.text = username
+                cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .none
+            }
+            else if (indexPath.row == 6){
+                cell.txtField.text = shortBio
+                cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .sentences
+            }
+            else if (indexPath.row == 7){
+                cell.txtField.text = hobby
+                cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .sentences
+            }
+            else if (indexPath.row == 8){
+                cell.txtField.text = userCountry
+                cell.txtField.keyboardType = .default
+                cell.txtField.autocapitalizationType = .words
+            }
+            else if (indexPath.row == 9){
+                cell.txtField.text = zipCode
+                cell.txtField.keyboardType = .numberPad
+                cell.txtField.autocapitalizationType = .none
             }
             
             return cell
@@ -211,6 +382,51 @@ extension EditProfileViewController: UIImagePickerControllerDelegate, UINavigati
 
 extension EditProfileViewController: EditProfileDelegate{
     func btnDoneTapped() {
-        self.dismiss(animated: true, completion: nil)
+        
+        if (firstName == ""){
+            Loaf(kFirstNameError, state: .error, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                
+            }
+            return
+        }
+        else if (lastName == ""){
+            Loaf(kLastNameError, state: .error, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                
+            }
+            return
+        }
+        else if (username == ""){
+            Loaf(kUsernameError, state: .error, location: .bottom, presentingDirection: .vertical, dismissingDirection: .vertical, sender: self).show(.custom(1.5)) { (handler) in
+                
+            }
+            return
+        }
+        
+        updateUserProfileWithRequest()
+        
+    }
+}
+
+extension EditProfileViewController: EditProfileGenderTableViewCellDelegate{
+    func genderTapped(gender: String) {
+        userGender = gender
+    }
+}
+
+extension EditProfileViewController: UITextFieldDelegate{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField.tag == 8){
+            textField.endEditing(true)
+            textField.resignFirstResponder()
+            let countryPicker = CountryPickerWithSectionViewController.presentController(on: self) { (country) in
+                self.userCountry = country.countryName
+                self.editProfileTableView.reloadData()
+            }
+            countryPicker.isCountryDialHidden = true
+            countryPicker.flagStyle = .circular
+            self.view.endEditing(true)
+            textField.endEditing(true)
+        }
+        
     }
 }
